9a2c752cb93544d9f8da5e4a80212d8d6d7b807a
Lisa Campbell <lisasoupcampbell@gmail.com>
2015-12-22 18:34:49 -0500

    Initial Commit

---
+++ iOSCourseRater/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iOSCourseRater
+//
+//  Created by Lisa Campbell on 12/22/15.
+//  Copyright (c) 2015 CourseRater. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
